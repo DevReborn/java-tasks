8c36433895b1fcd3b07f6879442c12a40bfe6c66
LukeReborn <91634842+DevReborn@users.noreply.github.com>
2022-06-13 15:19:41 +0100

    refactor(tasks): add tests and refactor

--- lib/src/main/java/com/reborn/tasks/DeferredValueTask.java
+++ lib/src/main/java/com/reborn/tasks/DeferredValueTask.java
@@ -1,117 +1,47 @@
 package com.reborn.tasks;
 
-import com.reborn.tasks.common.Pair;
-import com.reborn.tasks.exceptions.TaskException;
-
-import java.util.function.BiConsumer;
 import java.util.function.Consumer;
-import java.util.function.Function;
 
-public class DeferredValueTask<T> implements IDeferredValueTask<T> {
+public class DeferredValueTask<T> extends BaseTask<T> implements IDeferredValueTask<T> {
     private final Consumer<IDeferredValueTask<T>> _callable;
-    private final ITaskExecutor _executor;
-
-    protected T result;
-    protected boolean resultSet;
-    private Throwable _throwable;
-    private TaskState _state = TaskState.NOT_EXECUTED;
-
-    private Consumer<T> _onSuccess;
-    private Function<Throwable, Boolean> _onError;
-    private Consumer<Object> _onUpdate;
-    private BiConsumer<T, Boolean> _onComplete;
-    private Runnable _preExecute;
+    private T _result;
+    private boolean _resultSet;
 
     public DeferredValueTask(final Consumer<IDeferredValueTask<T>> callable,
                              final ITaskExecutor executor) {
+        super(executor);
         _callable = callable;
-        _executor = executor;
     }
-
-    protected void post(final Runnable postback) {
-        _executor.postback(postback);
-    }
-    protected void execute(final Runnable task) {
-        _executor.runTask(task);
+    public DeferredValueTask(final ITaskExecutor executor) {
+        super(executor);
+        _callable = null;
     }
 
     @Override
     public T getResult() {
         execute();
-        return result;
+        return _result;
     }
 
     public void setSucceeded(final T result) {
-        if(resultSet)
+        if(_resultSet)
             throw new IllegalStateException("cannot set success result more than once");
 
-        this.result = result;
-        resultSet = true;
+        _result = result;
+        _resultSet = true;
         if(_state == TaskState.EXECUTING) {
-            onResultSucceeded();
+            onResultSucceeded(result);
         }
     }
 
     public void setErrored(final Throwable throwable) {
-        if(_throwable != null)
+        if(_exception != null)
             throw new IllegalStateException("cannot set error result more than once");
 
-        _throwable = throwable;
+        _exception = throwable;
         if(_state == TaskState.EXECUTING) {
-            onResultErrored();
+            onResultErrored(_exception);
         }
-    }
-
-    @Override
-    public IValueTask<T> onSuccess(final Consumer<T> onSuccess) {
-        checkForValidState("alter");
-        if (_onSuccess == null) _onSuccess = onSuccess;
-        else _onSuccess = _onSuccess.andThen(onSuccess);
-        return this;
-    }
-
-    @Override
-    public IValueTask<T> onComplete(final BiConsumer<T, Boolean> onComplete) {
-        checkForValidState("alter");
-        if (_onComplete == null) _onComplete = onComplete;
-        else _onComplete = _onComplete.andThen(onComplete);
-        return this;
-    }
-
-    @Override
-    public ITask onExecuted(final Runnable onExecuted) {
-        checkForValidState("alter");
-        if (_preExecute == null) _preExecute = onExecuted;
-        else {
-            final Runnable oldPreExecute = _preExecute;
-            _preExecute = () -> {
-                oldPreExecute.run();
-                onExecuted.run();
-            };
-        }
-        return this;
-    }
-
-    @Override
-    public ITask onUpdate(final Consumer<Object> onUpdate) {
-        checkForValidState("alter");
-        if (_onUpdate == null) _onUpdate = onUpdate;
-        else _onUpdate = _onUpdate.andThen(onUpdate);
-        return this;
-    }
-
-    @Override
-    public ITask onError(final Function<Throwable, Boolean> onError) {
-        checkForValidState("alter");
-        if (_onError == null) _onError = onError;
-        else {
-            final Function<Throwable, Boolean> oldOnError = _onError;
-            _onError = throwable -> {
-                oldOnError.apply(throwable);
-                return onError.apply(throwable);
-            };
-        }
-        return this;
     }
 
     @Override
@@ -119,61 +49,12 @@ public class DeferredValueTask<T> implements IDeferredValueTask<T> {
         checkForValidState("re-execute");
 
         _state = TaskState.EXECUTING;
-        if(resultSet) {
+        if(_resultSet) {
             if(_preExecute != null) _preExecute.run();
-            onResultSucceeded();
+            onResultSucceeded(_result);
         } else {
             if (_preExecute != null) _preExecute.run();
-            _callable.accept(this);
-        }
-    }
-
-    @Override
-    public TaskState getState() {
-        return _state;
-    }
-
-    @Override
-    public Throwable getException() {
-        return _throwable;
-    }
-
-    private void checkForValidState(final String action) {
-        if(_state == TaskState.EXECUTING)
-            throw new IllegalStateException("Cannot " + action + " an already running task");
-        if(_state == TaskState.CANCELED)
-            throw new IllegalStateException("Cannot " + action + " an task that has been canceled");
-        if(_state == TaskState.ERRORED || _state == TaskState.SUCCEEDED)
-            throw new IllegalStateException("Cannot " + action + " an task that has already run.");
-    }
-
-    private void onResultSucceeded() {
-        if(_onSuccess != null || _onComplete != null) {
-            post(() -> {
-                _state = TaskState.SUCCEEDED;
-                if(_onSuccess != null) _onSuccess.accept(result);
-                if(_onComplete != null) _onComplete.accept(result, true);
-            });
-        } else {
-            _state = TaskState.SUCCEEDED;
-        }
-    }
-
-    private void onResultErrored() {
-        if(_onError != null || _onComplete != null) {
-            post(() -> {
-                _state = TaskState.ERRORED;
-                if(_onError != null) {
-                    final Boolean swallowException = _onError.apply(_throwable);
-                    if(!swallowException) {
-                        throw new TaskException(_throwable);
-                    }
-                }
-                if(_onComplete != null) _onComplete.accept(null, false);
-            });
-        } else {
-            _state = TaskState.ERRORED;
-            throw new TaskException(_throwable);
+            if (_callable != null) _callable.accept(this);
         }
     }
 }
---
+++ lib/src/test/java/com/reborn/tasks/ValueTaskTests.java
@@ -0,0 +1,115 @@
+package com.reborn.tasks;
+
+import com.reborn.tasks.exceptions.TaskException;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class ValueTaskTests {
+    private static final ITaskExecutor executor = new ITaskExecutor() {
+        @Override
+        public void runTask(final Runnable task) {
+            task.run();
+        }
+
+        @Override
+        public void postback(final Runnable postback) {
+            postback.run();
+        }
+    };
+
+    @Test
+    public void orderOfEvents() {
+        // Arrange
+        final ArrayList<Integer> messages = new ArrayList<>();
+        final ITask task = new ValueTask<>(op -> {
+            messages.add(1);
+            return null;
+        }, executor)
+        .onExecuted(() -> messages.add(0))
+        .onSuccess(() -> messages.add(2))
+        .onError(throwable -> {messages.add(3);})
+        .onComplete(() -> messages.add(4));
+
+        // Act
+        task.execute();
+
+        // Assert
+        Assertions.assertIterableEquals(Stream.of(0, 1, 2, 4).collect(Collectors.toList()), messages);
+    }
+
+    @Test
+    public void orderOfEvents_catchesException() {
+        // Arrange
+        final ArrayList<Integer> messages = new ArrayList<>();
+        final ITask task = new ValueTask<>(op -> {
+            throw new Exception("hello!");
+        }, executor)
+        .onExecuted(() -> messages.add(0))
+        .onError(throwable -> {messages.add(1);})
+        .onSuccess(() -> messages.add(2))
+        .onComplete(throwable -> messages.add(3));
+
+        // Act
+        task.execute();
+
+        // Assert
+        Assertions.assertIterableEquals(Stream.of(0, 1, 3).collect(Collectors.toList()), messages);
+    }
+
+    @Test
+    public void updateCalled_withObject() {
+        // Arrange
+        final ArrayList<String> messages = new ArrayList<>();
+        final ITask task = new ValueTask<>(op -> {
+            op.update("hello!");
+            return null;
+        }, executor)
+        .onUpdate(s -> messages.add((String) s));
+
+        // Act
+        task.execute();
+
+        // Assert
+        Assertions.assertIterableEquals(Stream.of("hello!").collect(Collectors.toList()), messages);
+    }
+
+    @Test
+    public void updateCalled_withFloatAndString() {
+        // Arrange
+        final ArrayList<String> messages = new ArrayList<>();
+        final ITask task = new ValueTask<>(op -> {
+            op.update(0.5f, "hello!");
+            return null;
+        }, executor)
+        .onUpdate((f, s) -> messages.add(f + " " + s));
+
+        // Act
+        task.execute();
+
+        // Assert
+        Assertions.assertIterableEquals(Stream.of("0.5 hello!").collect(Collectors.toList()), messages);
+    }
+
+    @Test
+    public void updateCalled_withObject_ThrowsWhenTryingToAccessViaFloatAndString() {
+        // Arrange
+        final ArrayList<String> messages = new ArrayList<>();
+        final ITask task = new ValueTask<>(op -> {
+            op.update("update the things!");
+            return null;
+        }, executor)
+        .onUpdate((f, s) -> messages.add(f + " " + s));
+
+        // Act
+        // Assert
+        final TaskException exception = Assertions.assertThrows(TaskException.class, task::execute);
+        Assertions.assertInstanceOf(IllegalStateException.class, exception.getCause());
+    }
+}
